44a960f54b952e93720f153a6ab70492e1cb2050
Mike DeLong <mikedelong@outlook.com>
2023-01-14 16:03:18 -0500

    feature: first version to get a response from an endpoint and turn it into a DataFrame

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ from logging import getLogger
 from pathlib import Path
 
 from arrow import now
+from requests import get
+from pandas import DataFrame
+
+
+
 
 OUTPUT_FOLDER = './data/'
+URL = 'https://api.usaspending.gov/'
 
 if __name__ == '__main__':
     TIME_START = now()
@@ -20,5 +26,8 @@ if __name__ == '__main__':
     for folder in [OUTPUT_FOLDER]:
         LOGGER.info('creating folder %s if it does not exist', folder)
         Path(folder).mkdir(parents=True, exist_ok=True)
+
+    response_json = get(url=URL + '/api/v2/references/agency/456/').json()
+    df = DataFrame(data=response_json)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
