9f02f2e26a7cfcaaf096e76742e932fd796ecd50
Mike DeLong <mikedelong@outlook.com>
2023-01-16 15:49:54 -0500

    feature: first working version to get results for agency numbers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-"""
+'''
 Get some data from the USASpending API
-"""
+'''
 
 from logging import INFO
 from logging import basicConfig
@@ -10,6 +10,7 @@ from pathlib import Path
 from arrow import now
 from pandas import DataFrame
 from requests import get
+from requests import post
 
 OUTPUT_FOLDER = './data/'
 URL = 'https://api.usaspending.gov/'
@@ -24,7 +25,11 @@ if __name__ == '__main__':
         LOGGER.info('creating folder %s if it does not exist', folder)
         Path(folder).mkdir(parents=True, exist_ok=True)
 
-    response_json = get(url=URL + '/api/v2/references/agency/456/').json()
-    df = DataFrame(data=response_json)
+    # found values for agency are 400? and 456
 
+    data = {agency:
+                get(url=URL + '/api/v2/references/agency/{}/'.format(agency)).json()['results']
+            for agency in range(410, 500)
+            }
+    df = DataFrame(data=data).T.drop_duplicates()
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
