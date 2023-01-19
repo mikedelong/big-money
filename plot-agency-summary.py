d8e5d1b4cac34ba653d382f9fb77cac9ba0dcefa
Mike DeLong <mikedelong@outlook.com>
2023-01-19 16:36:02 -0500

    feature: drop the agencies with no money

--- plot-agency-summary.py
+++ plot-agency-summary.py
@@ -33,6 +33,9 @@ if __name__ == '__main__':
     LOGGER.info('loading: %s', filepath_or_buffer)
     df = read_csv(filepath_or_buffer=filepath_or_buffer, usecols=USECOLS)
 
+    # drop agencies with no money
+    df = df[df['outlay_amount'] > 0]
+
     outlay_amount = df['outlay_amount'].sum()
     obligated_amount = df['obligated_amount'].sum()
     current_total = df['current_total_budget_authority_amount'].sum()
@@ -40,6 +43,8 @@ if __name__ == '__main__':
 
     fractions = [item / authority_amount for item in [outlay_amount, obligated_amount, current_total]]
     columns = ['outlay_amount', 'obligated_amount', 'budget_authority_amount']
-    plot = df[columns].plot.pie(subplots=True, figsize=(11, 6))
+
+    plot = df[columns].plot.pie(subplots=True, figsize=(11, 6), labels=df['agency_name'])
     savefig(fname=OUTPUT_FOLDER + 'triple_plot.png', format='png')
+
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
