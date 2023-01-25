49d5ed5addf5909338e7ea3e14716d3aacecfd4a
Mike DeLong <mikedelong@outlook.com>
2023-01-25 13:47:33 -0500

    feature: write the data to a JSON file; it is a nested JSON document, and there's no way to turn it into a DataFrame

--- get-budget-function-list.py
+++ get-budget-function-list.py
@@ -2,6 +2,7 @@
 Get some data from the USASpending API
 """
 
+from json import dump
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
@@ -31,5 +32,10 @@ if __name__ == '__main__':
         url = 'https://api.usaspending.gov/api/v2/agency/{}/budget_function/'.format(agency)
         data[agency] = get(url=url).json()
         LOGGER.info('%s %d', agency, len(data[agency]))
+
+    output_file = OUTPUT_FOLDER + 'get-budget-function-list.json'
+    LOGGER.info('dumping %d records to JSON: %s', len(data), output_file)
+    with open(file=output_file, mode='w') as output_fp:
+        dump(obj=data, fp=output_fp, sort_keys=True, indent=4)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
