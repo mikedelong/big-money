ff0413a6f23031d05157e78af69cef632ea43220
Mike DeLong <mikedelong@outlook.com>
2023-01-31 14:48:32 -0500

    feature: a first working version using the bulk download endpoint

--- demo_post.py
+++ demo_post.py
@@ -7,6 +7,7 @@ from logging import basicConfig
 from logging import getLogger
 
 from arrow import now
+from pandas import concat
 from pandas import json_normalize
 from requests import post
 
@@ -17,13 +18,17 @@ DATA = [
     {
         'search_text': 'Award'
     },
-][1]
-FIELD = ['results', 'matched_terms'][1]
+    {
+        'type': 'award_agencies'
+        # 'agency': 1
+    },
+][2]
+FIELD = ['results', 'matched_terms', 'agencies'][2]
 URL = [
     'https://api.usaspending.gov/api/v2/autocomplete/funding_agency/',
     'https://api.usaspending.gov/api/v2/autocomplete/glossary/',
-][1]
-
+    'https://api.usaspending.gov/api/v2/bulk_download/list_agencies/',
+][2]
 
 if __name__ == '__main__':
     TIME_START = now()
@@ -38,7 +43,11 @@ if __name__ == '__main__':
     response = post(url=URL, data=DATA, )
     LOGGER.info('status code: %d', response.status_code)
     LOGGER.info('reason: %s', response.reason)
-    response_json = response.json()
-    df = json_normalize(data=response_json[FIELD])
+    if response.status_code == 200:
+        response_json = response.json()
+        if FIELD != 'agencies':
+            df = json_normalize(data=response_json[FIELD])
+        else:
+            df = concat([json_normalize(data=response_json[FIELD][item]) for item in response_json[FIELD].keys()])
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
