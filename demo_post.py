923b530ed1eba2b53c3b45ee284c7cb189e73f12
Mike DeLong <mikedelong@outlook.com>
2023-02-01 14:26:43 -0500

    feature: first working version to use the bulk download

--- demo_post.py
+++ demo_post.py
@@ -10,6 +10,7 @@ from arrow import now
 from pandas import concat
 from pandas import json_normalize
 from requests import post
+from requests import get
 
 INDEX = 3
 DATA = [
@@ -37,33 +38,31 @@ DATA = [
                     'type': 'funding'
                 }
             ],
-            'prime_and_sub_award_types': {
-                'prime_awards': [
-                    '02',
-                    '03',
-                    '04',
-                    '05',
-                    '06',
-                    '07',
-                    '08',
-                    '09',
-                    '10',
-                    '11',
-                    'A',
-                    'B',
-                    'C',
-                    'D',
-                    'IDV_A',
-                    'IDV_B',
-                    'IDV_B_A',
-                    'IDV_B_B',
-                    'IDV_B_C',
-                    'IDV_C',
-                    'IDV_D',
-                    'IDV_E'
-                ],
-                'sub_awards': []
-            },
+            'prime_award_types': [
+                # '02',
+                # '03',
+                # '04',
+                # '05',
+                # '06',
+                # '07',
+                # '08',
+                # '09',
+                # '10',
+                # '11',
+                'A'  # ,
+                # 'B',
+                # 'C',
+                # 'D',
+                # 'IDV_A',
+                # 'IDV_B',
+                # 'IDV_B_A',
+                # 'IDV_B_B',
+                # 'IDV_B_C',
+                # 'IDV_C',
+                # 'IDV_D',
+                # 'IDV_E'
+            ],
+            # 'sub_award_types': [],
             'date_range': {
                 'start_date': '2019-10-01',
                 'end_date': '2020-09-30'
@@ -72,7 +71,8 @@ DATA = [
         'request_type': 'award'
     }
 ][INDEX]
-FIELD = ['results', 'matched_terms', 'agencies', 'foo'][INDEX]
+FIELD = ['results', 'matched_terms', 'agencies', 'download'][INDEX]
+OUTPUT_FOLDER = './data/'
 URL = [
     'https://api.usaspending.gov/api/v2/autocomplete/funding_agency/',
     'https://api.usaspending.gov/api/v2/autocomplete/glossary/',
@@ -90,14 +90,22 @@ if __name__ == '__main__':
 
     LOGGER.info('url: %s', URL)
     LOGGER.info('data: %s', DATA)
-    # response = post(url=URL, data=DATA, )
     response = post(url=URL, json=DATA, )
     LOGGER.info('status code: %d', response.status_code)
     LOGGER.info('reason: %s', response.reason)
     if response.status_code == 200:
         response_json = response.json()
-        if FIELD != 'agencies':
+        if FIELD == 'agengies':
+            df = concat([json_normalize(data=response_json[FIELD][item]) for item in response_json[FIELD].keys()])
+        elif FIELD in {'results', 'matched_terms'}:
             df = json_normalize(data=response_json[FIELD])
+        elif FIELD == 'download':
+            output_file = OUTPUT_FOLDER + response_json['file_name']
+            LOGGER.info('writing to %s', output_file)
+            url = response_json['file_url']
+            with open(file=output_file, mode='wb') as output_fp:
+                get_content = get(url=url, stream=True).content
+                output_fp.write(get_content)
         else:
             df = concat([json_normalize(data=response_json[FIELD][item]) for item in response_json[FIELD].keys()])
 
