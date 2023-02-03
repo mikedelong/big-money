5894f306fb6b6f9df612c37371f88edd07e7fd6e
Mike DeLong <mikedelong@outlook.com>
2023-02-03 16:08:06 -0500

    feature: first working version to get the data we want

---
+++ read_first_dataset.py
@@ -0,0 +1,35 @@
+"""
+Get some data from the USASpending API
+"""
+
+from glob import glob
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+from pathlib import Path
+
+from arrow import now
+from pandas import read_csv
+
+INPUT_FOLDER = './data/'
+
+if __name__ == '__main__':
+    TIME_START = now()
+    LOGGER = getLogger(__name__, )
+    basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
+    if LOGGER.hasHandlers():
+        LOGGER.handlers.clear()
+    LOGGER.info('started')
+
+    for folder in [INPUT_FOLDER]:
+        LOGGER.info('creating folder %s if it does not exist', folder)
+        Path(folder).mkdir(parents=True, exist_ok=True)
+
+    result = dict()
+    for index, input_file in enumerate(glob(INPUT_FOLDER + '*/*.csv')):
+        if index == 0:
+            LOGGER.info(input_file)
+            df  =  read_csv(filepath_or_buffer=input_file)
+
+
+    LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
