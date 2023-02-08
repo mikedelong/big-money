d9f2fdb02551ff11110c469aea59292fa41ae979
Mike DeLong <mikedelong@outlook.com>
2023-02-07 19:35:50 -0500

    feature: first working implementation

---
+++ get_naics.py
@@ -0,0 +1,34 @@
+"""
+Get some data from the USASpending API
+"""
+
+from glob import glob
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+from pathlib import Path
+
+from arrow import now
+from pandas import read_csv
+from requests import get
+
+INPUT_FOLDER = './data/'
+URL = 'https://api.usaspending.gov/api/v2/references/naics/'
+
+if __name__ == '__main__':
+    TIME_START = now()
+    LOGGER = getLogger(__name__, )
+    basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
+    if LOGGER.hasHandlers():
+        LOGGER.handlers.clear()
+    LOGGER.info('started')
+
+    for folder in [INPUT_FOLDER]:
+        LOGGER.info('creating folder %s if it does not exist', folder)
+        Path(folder).mkdir(parents=True, exist_ok=True)
+
+    result = get(url=URL)
+    if result.status_code == 200:
+        result_json = result.json()
+
+    LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
